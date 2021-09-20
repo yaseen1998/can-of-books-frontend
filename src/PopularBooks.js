f4d7ccf9673ef7211bd4d11e250667f14cf19935
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-09-21 01:27:39 +0300

    edit

--- src/PopularBooks.js
+++ src/PopularBooks.js
@@ -2,25 +2,83 @@ import axios from "axios";
 import React, { Component } from "react";
 import { Carousel, Caption } from "react-bootstrap";
 import style from "./style.css";
+import Addform from "./components/Addform";
 class PopularBooks extends Component {
   constructor(props) {
     super(props);
     this.state = {
       bookData: [],
+      showform: false,
+      showaddbutton:true
     };
   }
+
   componentDidMount = () => {
     axios
-      .get(`http://localhost:8000/books2?id=61486c74791525e44115a7a8`)
+      .get(`https://yaseen-booksbackend.herokuapp.com/books`)
       .then((Response) => {
         this.setState({
-          bookData: Response.data.Books,
+          bookData: Response.data,
         });
       });
+  };
+
+  handleSubmit = async (values) => {
+    const booklist = {
+      title: values.title,
+      description: values.description,
+      status: values.status,
+      email: values.email,
+    };
+    let books = await axios.post(`http://localhost:8000/create-book`, booklist);
+    this.setState({
+      bookData: books.data,
+    });
+  };
+  handleDelete = async (id) => {
+    let bookid = id;
+    let deletebook = await axios.delete(
+      `http://localhost:8000/delete-book/${bookid}`
+    );
+    this.setState({
+      bookData: deletebook.data,
+    });
+  };
+  showform = () => {
+    this.setState({
+      showform: true,
+      showaddbutton:false
+    });
+  };
+  closeform = () => {
+    this.setState({
+      showform: false,
+      showaddbutton:true
+    });
   };
   render() {
     return (
       <div>
+        <div className='mainForm'>
+          {this.state.showaddbutton &&
+          <button type="button" onClick={this.showform}>
+            {" "}
+            add book
+          </button>
+  }
+          <br/>
+            <hr/>
+          {this.state.showform && <Addform handleSubmit={this.handleSubmit} />}
+          <br/>
+            <hr/>
+            {this.state.showform &&
+          <button type="button" onClick={this.closeform}>
+             {" "}
+             <br/>
+            close
+          </button>}
+        </div>
+
         {this.state.bookData.length > 0 && (
           <Carousel indicators={false} className="Carousel">
             {this.state.bookData.map((Element, i) => {
@@ -36,6 +94,11 @@ class PopularBooks extends Component {
                     <p className="ele">Description :{Element.description}</p>
                     <p className="ele">Status :{Element.status}</p>
                     <p className="ele">E-mail :{Element.email}</p>
+                    <button onClick={() => this.handleDelete(Element._id)}>
+                      {" "}
+                      remove item
+                    </button>
+                    {/* {this.setState({key:Element._id})} */}
                   </Carousel.Caption>
                 </Carousel.Item>
               );
